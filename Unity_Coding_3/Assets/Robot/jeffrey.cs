251bc81b71336e14df1613c5c5f9e15166f33761
Alyssa Rubenstein <alyssarubenstein@wireless-10-104-160-199.umd.edu>
2023-10-26 15:19:09 -0400

    Added gui stuff- alyssa

---
+++ Unity_Coding_3/Assets/Robot/jeffrey.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class jeffrey : MonoBehaviour
+{
+    TMP_Text Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Text = this.GetComponent<TMP_Text>();
+        Text.text = "Hello World";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
